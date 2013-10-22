132af6a66b053bd1e7069ca8456d9035177ab359
Jason Lu <jasonl.biz@gmail.com>
2013-10-22 13:22:59 -0400

    Initial Commit

---
+++ CS341-midterm/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CS341-midterm
+//
+//  Created by Jason Lu on 10/22/13.
+//  Copyright (c) 2013 Lu Jason. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
